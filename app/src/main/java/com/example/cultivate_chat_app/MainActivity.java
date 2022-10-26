1089ec2776cfe7421c48ce92df53b4d4a65fec3c
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-10-25 19:42:02 -0700

    created auth and main nav graphs

--- app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
+++ app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cultivate_chat_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      Log.i("MainActivity", "The onCreate method has ran.");
    }
 }
