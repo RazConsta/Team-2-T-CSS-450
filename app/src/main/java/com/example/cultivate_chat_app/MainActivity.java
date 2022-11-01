8de809a51a2ad364de26f7e1345fde350326261e
Raz Consta <66283215+RazConsta@users.noreply.github.com>
2022-11-01 02:29:41 -0700

    Added navigation bar to add pages of the app + fixed register and login pages

--- app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
+++ app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.cultivate_chat_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.util.Log;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+   private AppBarConfiguration mAppBarConfiguration;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      Log.i("MainActivity", "The onCreate method has ran.");
+      // Make sure the new statements go BELOW setContentView
+      BottomNavigationView navView = findViewById(R.id.nav_view);
+      // Passing each menu ID as a set of Ids because each
+      // menu should be considered as top level destinations.
+      mAppBarConfiguration = new AppBarConfiguration.Builder(
+              R.id.homeFragment, R.id.chatsFragment, R.id.contactsFragment, R.id.weatherFragment, R.id.registerFragment)
+              .build();
+      NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+      NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+      NavigationUI.setupWithNavController(navView, navController);
    }
 }
--- app/src/main/java/com/example/cultivate_chat_app/ui/chats/ChatsFragment.java
+++ app/src/main/java/com/example/cultivate_chat_app/ui/chats/ChatsFragment.java
@@ -1,4 +1,4 @@
-package com.example.cultivate_chat_app.ui.register;
+package com.example.cultivate_chat_app.ui.chats;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.cultivate_chat_app.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
+ * Use the {@link ChatsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegisterFragment extends Fragment {
+public class ChatsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class RegisterFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RegisterFragment() {
+    public ChatsFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class RegisterFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
+     * @return A new instance of fragment ChatsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
+    public static ChatsFragment newInstance(String param1, String param2) {
+        ChatsFragment fragment = new ChatsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return inflater.inflate(R.layout.fragment_chats, container, false);
     }
 }
