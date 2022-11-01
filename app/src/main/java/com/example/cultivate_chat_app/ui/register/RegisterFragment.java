860f1f99802b4396e9cbd2c6e5ba5eb2fb8a7988
Tien Dao <tienminh1351@gmail.com>
2022-10-31 23:12:45 -0700

    create register interface with no functionality

---
+++ app/src/main/java/com/example/cultivate_chat_app/ui/register/RegisterFragment.java
@@ -0,0 +1,24 @@
+package com.example.cultivate_chat_app.ui.register;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cultivate_chat_app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
