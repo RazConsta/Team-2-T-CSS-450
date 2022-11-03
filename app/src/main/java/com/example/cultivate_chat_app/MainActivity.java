444ece0a5abd859377d693eb0042d88da7d8d64b
Tien Dao <tienminh1351@gmail.com>
2022-11-02 22:05:27 -0700

    create a login button and have the register button navigate to RegisterFragment.

--- app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
+++ app/src/main/java/com/example/cultivate_chat_app/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
       // Passing each menu ID as a set of Ids because each
       // menu should be considered as top level destinations.
       mAppBarConfiguration = new AppBarConfiguration.Builder(
-              R.id.homeFragment, R.id.chatsFragment, R.id.contactsFragment, R.id.weatherFragment, R.id.registerFragment)
+              R.id.homeFragment, R.id.chatsFragment, R.id.contactsFragment, R.id.weatherFragment, R.id.signInFragment)
               .build();
       NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
       NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
--- app/src/main/java/com/example/cultivate_chat_app/ui/authorization/signin/SignInFragment.java
+++ app/src/main/java/com/example/cultivate_chat_app/ui/authorization/signin/SignInFragment.java
@@ -2,26 +2,52 @@ package com.example.cultivate_chat_app.ui.authorization.signin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cultivate_chat_app.R;
+import com.example.cultivate_chat_app.databinding.FragmentSignInBinding;
 
 public class SignInFragment extends Fragment {
+
+   private FragmentSignInBinding mBinding;
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-
    }
 
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                             Bundle savedInstanceState) {
       // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_sign_in, container, false);
+      mBinding = FragmentSignInBinding.inflate(inflater, container, false);
+      return mBinding.getRoot();
    }
+
+   @Override
+   public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+      //Use a Lamda expression to add the OnClickListener
+      mBinding.buttonToRegister.setOnClickListener(button -> processRegister());
+   }
+
+   @Override
+   public void onDestroyView() {
+      super.onDestroyView();
+      mBinding = null;
+   }
+
+   private void processRegister() {
+      Log.d("REGISTER", "Process to register");
+      Navigation.findNavController(getView()).navigate(R.id.action_signInFragment_to_registerFragment);
+   }
+
 }
