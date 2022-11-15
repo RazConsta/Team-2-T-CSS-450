38498b451affbc565d5b68fb11f23e7cb89bd973
Qinyu <taoqinyu219@gmail.com>
2022-11-15 14:19:09 -0800

    Implemented navigation on chat and option button in contact card

---
+++ app/src/main/java/com/example/cultivate_chat_app/ui/chats/CreateNewChatFragment.java
@@ -0,0 +1,26 @@
+package com.example.cultivate_chat_app.ui.chats;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cultivate_chat_app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateNewChatFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_new_chat, container, false);
+    }
+}
--- app/src/main/java/com/example/cultivate_chat_app/ui/contacts/ContactRecyclerViewAdapter.java
+++ app/src/main/java/com/example/cultivate_chat_app/ui/contacts/ContactRecyclerViewAdapter.java
@@ -2,12 +2,16 @@ package com.example.cultivate_chat_app.ui.contacts;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cultivate_chat_app.R;
@@ -49,6 +53,36 @@ public class ContactRecyclerViewAdapter extends RecyclerView.Adapter<ContactRecy
         holder.nickname.setText(mContacts.get(position).getNickname());
         holder.fullName.setText(mContacts.get(position).getFirstname()
                 +" "+mContacts.get(position).getLastname());
+
+        // contact card message button action
+        holder.messageButton.setOnClickListener(Navigation.createNavigateOnClickListener
+                (R.id.action_contactsFragment_to_createNewChatFragment));
+        holder.messageButton.setOnClickListener(Navigation.createNavigateOnClickListener
+                (R.id.action_addFriendsFragment_to_createNewChatFragment));
+
+        // contact card option button action
+        holder.optionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(mContext, holder.messageButton);
+                popup.inflate(R.menu.concact_card_menu);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.add_friend:
+                                //handle addfriend click
+                                break;
+                            case R.id.remove_friend:
+                                //handle remove friend click
+                                break;
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
+
     }
 
 
@@ -59,12 +93,15 @@ public class ContactRecyclerViewAdapter extends RecyclerView.Adapter<ContactRecy
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView nickname, fullName;
+        ImageView messageButton, optionButton;
         ConstraintLayout cardLayout;
         View view;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             nickname = itemView.findViewById(R.id.contact_card_nickname);
             fullName = itemView.findViewById(R.id.contact_card_fullname);
+            messageButton = itemView.findViewById(R.id.contact_card_message_friend_button);
+            optionButton = itemView.findViewById(R.id.contact_card_option_button);
             cardLayout = itemView.findViewById(R.id.contact_card_root);
             view = itemView.getRootView();
         }
