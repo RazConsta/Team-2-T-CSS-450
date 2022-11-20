5a251fba3718755b0e8db1f165a4162dc89be9a8
Qinyu <taoqinyu219@gmail.com>
2022-11-19 23:43:54 -0800

    Implemented search/accept/send requests

---
+++ app/src/main/java/com/example/cultivate_chat_app/ui/contacts/ContactListViewModel.java
@@ -0,0 +1,175 @@
+package com.example.cultivate_chat_app.ui.contacts;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A simple {@link AndroidViewModel} subclass.
+ */
+public class ContactListViewModel extends AndroidViewModel {
+    private MutableLiveData<List<Contact>> mContactList;
+    private MutableLiveData<List<Contact>> mPendingList;
+
+    /**
+     * Constructor
+     * @param application current Application
+     */
+    public ContactListViewModel(@NonNull Application application) {
+        super(application);
+        mContactList = new MutableLiveData<>();
+        mContactList.setValue(new ArrayList<>());
+        mPendingList = new MutableLiveData<>();
+        mPendingList.setValue(new ArrayList<>());
+    }
+
+    /**
+     * Add observer for contacts
+     * @param owner owner
+     * @param observer observer
+     */
+    public void addContactListObserver(@NonNull LifecycleOwner owner,
+                                       @Nullable Observer<?super List<Contact>> observer){
+        mContactList.observe(owner,observer);
+    }
+
+    /**
+     * Add observer for requests
+     * @param owner owner
+     * @param observer observer
+     */
+    public void addPendingListObserver(@NonNull LifecycleOwner owner,
+                                       @Nullable Observer<?super List<Contact>> observer){
+        mPendingList.observe(owner,observer);
+    }
+
+    /**
+     * reset contacts list
+     */
+    public void resetContacts(){
+        mContactList.setValue(new ArrayList<>());
+    }
+
+    /**
+     * reset requests list
+     */
+    public void resetRequests() {
+        mPendingList.setValue(new ArrayList<>());
+    }
+
+    /**
+     * Add to pending requests list
+     * @param contact contact to be added
+     */
+    public void addToPendingList(Contact contact) {
+        mPendingList.getValue().add(contact);
+        mPendingList.setValue(mPendingList.getValue());
+    }
+
+    /**
+     * handle error from server
+     * @param error error
+     */
+    protected void handleError(final VolleyError error) {
+        throw new IllegalStateException(error.getMessage());
+    }
+
+    /**
+     * Handle result from the server
+     * @param result result from server
+     * @param type the type or data
+     */
+    protected void handleResult(final JSONObject result, String type) {
+        MutableLiveData<List<Contact>> list = mContactList;
+        try {
+            JSONObject response = result;
+            FriendStatus status = FriendStatus.FRIENDS;
+
+            Log.d("TTT", type);
+
+            if (type.equals("requests")) {
+                status = FriendStatus.RECEIVED_REQUEST;
+                list = mPendingList;
+            }
+
+            if (response.has("rows")) {
+                JSONArray rows = response.getJSONArray("rows");
+                for (int i = 0; i< rows.length(); i++){
+                    JSONObject jsonContact = rows.getJSONObject(i);
+                    Contact contact = new Contact(
+                            jsonContact.getString("id"),
+                            jsonContact.getString("nickname"),
+                            jsonContact.getString("firstname"),
+                            jsonContact.getString("lastname"),
+                            jsonContact.getString("email"),
+                            status
+                    );
+
+                    if(!list.getValue().contains(contact))
+                        list.getValue().add(contact);
+                }
+            } else {
+                Log.e("ERROR", "No Friends Provided");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("ERROR",e.getMessage());
+        }
+        list.setValue(list.getValue());
+    }
+
+    /**
+     * Connect to server for contacts
+     * @param memberId member ID
+     * @param jwt jwt of the user
+     * @param type type of contacts
+     */
+    public void connectContacts(int memberId, String jwt, String type) {
+        String url = "https://cultivate-app-web-service.herokuapp.com"
+                + "friendsList/" + memberId + "/";
+
+        Request<JSONObject> request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                result -> handleResult(result, type),
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/cultivate_chat_app/ui/contacts/SearchViewModel.java
@@ -0,0 +1,137 @@
+package com.example.cultivate_chat_app.ui.contacts;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A simple {@link AndroidViewModel} subclass.
+ */
+public class SearchViewModel extends AndroidViewModel {
+    private MutableLiveData<List<Contact>> mSearchList;
+
+    /**
+     * Constructor
+     * @param application current application
+     */
+    public SearchViewModel(@NonNull Application application) {
+        super(application);
+        mSearchList = new MutableLiveData<>();
+        mSearchList.setValue(new ArrayList<>());
+    }
+
+    /**
+     * Add observer for searched contacts list
+     * @param owner owner
+     * @param observer observer
+     */
+    public void addSearchListObserver(@NonNull LifecycleOwner owner,
+                                      @Nullable Observer<?super List<Contact>> observer){
+        mSearchList.observe(owner,observer);
+    }
+
+    /**
+     * Reset the searched list
+     */
+    private void resetSearchResults() {
+        mSearchList.setValue(new ArrayList<>());
+    }
+
+    /**
+     * Search add members
+     * @param jwt jwt of the user
+     * @param searched starting username of the members
+     */
+    public void connectSearch(String jwt, String searched) {
+        resetSearchResults();
+
+        String url = "https://cultivate-app-web-service.herokuapp.com"
+                + "search/" + searched;
+        Request request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                this::handleResult,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                // anything works for the jwt for now
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+
+    /**
+     * Handle error from the server
+     * @param error error from the server
+     */
+    protected void handleError(final VolleyError error) {
+        Log.e("ERROR", error.getLocalizedMessage());
+    }
+
+    /**
+     * Handle result from the server
+     * @param result result from the server
+     */
+    protected void handleResult(final JSONObject result) {
+        try {
+            JSONObject response = result;
+            FriendStatus status = FriendStatus.NOT_FRIENDS;
+
+            if (response.has("rows")) {
+                JSONArray rows = response.getJSONArray("rows");
+                for (int i = 0; i< rows.length(); i++){
+                    JSONObject jsonContact = rows.getJSONObject(i);
+                    Contact contact = new Contact(
+                            jsonContact.getString("id"),
+                            jsonContact.getString("nickname"),
+                            jsonContact.getString("firstname"),
+                            jsonContact.getString("lastname"),
+                            jsonContact.getString("email"),
+                            status
+                    );
+
+                    if(!mSearchList.getValue().contains(contact))
+                        mSearchList.getValue().add(contact);
+                }
+            } else {
+                Log.e("ERROR", "No Friends Provided");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("ERROR",e.getMessage());
+        }
+        mSearchList.setValue(mSearchList.getValue());
+    }
+}
